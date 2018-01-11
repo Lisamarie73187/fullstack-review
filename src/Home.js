b3dca3328b97db0b2de83f83c2a0ba9e2d093921
Lisa Herzberg <lisamarieherzberg@gmail.com>
2018-01-11 12:10:22 -0700

    Home Auth0

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react'
 import logo from './communityBank.svg'
+import Auth0Lock from 'auth0-lock'
 
 
 class Home extends Component {
-    constructor(props) {
-        super(props)
+    constructor() {
+        super()
         this.state = {
+
         }
+        this.lock = null
+        this.login = this.login.bind(this)
     }
 
-    login (){
-        
+    componentDidMount(){
+        this.lock= new Auth0Lock(process.env.REACT_APP_AUTH0_CLIENT_ID,
+        process.env.REACT_APP_AUTH0_DOMAIN)
+        this.lock.on('authenticated', authResult => {
+            this.lock.getUserInfo(authResult.accessToken, (error, user) => {
+
+            })
+        })
+    }
+    
+    login(){
+        this.lock.show();
     }
 
     render() {
+
         return (
             <div className="layout">
                 <div><img src={logo} alt="logo" width="300px"/></div>
